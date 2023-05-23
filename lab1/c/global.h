5fc307ab8bf0e6581ed6062bb22e3210545b6bf4
samuelzureick <83126355+samuelzureick@users.noreply.github.com>
2023-05-23 15:21:33 +0100

    Delete lab1 directory

--- lab1/c/global.h
+++
@@ -1,6 +0,0 @@
-
-extern char* prog_name;
-extern int verbose;
-extern int mode;
-
-void check (void *memory);
--- lab1/python/speller_darray.py
+++
@@ -1,8 +0,0 @@
-import speller
-import sys
-import config
-
-config.set_type = config.SetType.DARRAY
-config.prog_name = "speller_darray.py"
-
-speller.spelling(sys.argv)
